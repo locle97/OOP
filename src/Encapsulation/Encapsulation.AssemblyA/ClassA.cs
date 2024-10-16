8c9cde77d9a2a6cade1ddbc6ba5f247b997413f9
Loc Le <loclep1@VNNOT01132.>
2024-10-16 17:00:51 +0700

    feat - update README and folder structure for Encapsulation

---
+++ src/Encapsulation/Encapsulation.AssemblyA/ClassA.cs
@@ -0,0 +1,16 @@
+﻿namespace Encapsulation.AssemblyA;
+
+public class ClassA
+{
+    internal ClassAInternal AInternal;
+
+    public ClassA()
+    {
+        AInternal = new ClassAInternal();
+    }
+}
+
+internal class ClassAInternal
+{
+
+}
---
+++ src/Encapsulation/Encapsulation.AssemblyB/ClassB.cs
@@ -0,0 +1,13 @@
+﻿using Encapsulation.AssemblyA;
+
+namespace Encapsulation.AssemblyB;
+
+public class ClassB
+{
+    private readonly ClassA _a;
+
+    public ClassB()
+    {
+        _a = new ClassA();
+    }
+}
